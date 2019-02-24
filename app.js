4675c80d645fa8d0074d662d6d712b795f83cfd1
dbachinin <dbachinin@gmail.com>
2019-02-24 18:10:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunch_menu = "";
+  $scope.LunchCheckState = "";
+  $scope.textColorClass = "";
+  $scope.borderColor = 'red';
+
+  $scope.countLunchItems = () => {
+    $scope.LunchCheckState = countLunchItems($scope.lunch_menu);
+  };
+  function countLunchItems(items) {
+    let items_count = items.split(',').filter(() => {
+      return val != '';
+    }).length;
+    let out = "";
+    $scope.borderColor = 'green';
+    if (items_count <= 3) {
+      out = 'Enjoy';
+      $scope.textColorClass = "text-info";
+    }else{
+      out = 'Too much';
+      $scope.textColorClass = "text-danger";
+    }
+    return out
+  }
+}
+
+})();
