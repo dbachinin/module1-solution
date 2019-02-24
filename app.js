703f8afe278658e2e98494e21c16e08036a0abd7
dbachinin <dbachinin@gmail.com>
2019-02-24 18:57:59 +0300

    fix regexp

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ function LunchCheckController($scope) {
   $scope.lunch_menu = "";
   $scope.LunchCheckState = "";
   $scope.textColorClass = "";
-  $scope.borderColor = 'red';
+  $scope.borderColor = '';
 
   $scope.countLunchItems = () => {
     $scope.LunchCheckState = countLunchItems($scope.lunch_menu);
   };
   function countLunchItems(items) {
-    let items_count = items.split(',').filter((val) => {return val != '';}).length;
+    let items_count = items.split(',').filter((val) => {return !val.match(/^ *$/);}).length;
     let out = "";
     if (items_count == 0) {
       $scope.textColorClass = "text-danger";
       out = 'Please enter data first!';
+      $scope.borderColor = 'red';
     }else{
       $scope.borderColor = 'green';
       if (items_count <= 3) {
@@ -27,7 +28,7 @@ function LunchCheckController($scope) {
         $scope.textColorClass = "text-info";
       }else{
         out = 'Too much';
-        $scope.textColorClass = "text-danger";
+        $scope.textColorClass = "text-info";
       }
     }
     return out
